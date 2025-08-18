11f4a902a9d1ad87ed5d6519a6e9f8360b6087ee
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2025-08-19 00:42:29 +0100

    [#552] Fixed all linting errors in `DisplayCAL.jsondict` module.

--- DisplayCAL/jsondict.py
+++ DisplayCAL/jsondict.py
@@ -4,6 +4,8 @@ It allows efficient parsing and manipulation of JSON files by loading data only
 when accessed.
 """
 
+from typing import TextIO
+
 import demjson_compat
 
 from DisplayCAL.lazydict import LazyDict
@@ -12,11 +14,11 @@ from DisplayCAL.lazydict import LazyDict
 class JSONDict(LazyDict):
     """JSON lazy dictionary."""
 
-    def parse(self, fileobj):
+    def parse(self, fileobj: TextIO) -> None:
         """Parse the JSON data from the given file object.
 
         Args:
-            fileobj (file-like object): A file-like object containing JSON
+            fileobj (TextIO): A file-like object containing JSON
                 data.
         """
         dict.update(self, demjson_compat.decode(fileobj.read()))
