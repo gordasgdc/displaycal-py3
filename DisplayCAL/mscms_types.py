4ed526c3a6bad58839c534f82d16732c2b344e45
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2026-02-27 22:44:07 +0000

    [#552] Fixed linting errors in `mscms_types` module.

--- DisplayCAL/mscms_types.py
+++ DisplayCAL/mscms_types.py
@@ -1,16 +1,22 @@
-from enum import auto
-from enum import IntEnum
-from enum import IntFlag
+"""Classes for the various enums used in the Windows Color System API.
 
-class dwDeviceClass(IntEnum):
-    """Available device classes to be used in the dwDeviceClass field"""
+There are also some helper functions to convert between enum values and their
+string representations.
+"""
+
+from enum import IntEnum, IntFlag, auto
+
+
+class dwDeviceClass(IntEnum):  # noqa: N801
+    """Available device classes to be used in the dwDeviceClass field."""
 
     CLASS_SCANNER = int.from_bytes(b"scnr", byteorder="big")
     CLASS_MONITOR = int.from_bytes(b"mntr", byteorder="big")
     CLASS_PRINTER = int.from_bytes(b"prtr", byteorder="big")
 
-class dwFieldsUsed(IntFlag):
-    """Available fields to be used in the ENUMTYPEW structure"""
+
+class dwFieldsUsed(IntFlag):  # noqa: N801
+    """Available fields to be used in the ENUMTYPEW structure."""
 
     ET_DEVICENAME = 0x00000001
     ET_MEDIATYPE = 0x00000002
@@ -30,12 +36,17 @@ class dwFieldsUsed(IntFlag):
     ET_CREATOR = 0x00008000
     ET_DEVICECLASS = 0x00010000
 
-class WCS_PROF_SCOPE(IntEnum):
+
+class WCS_PROF_SCOPE(IntEnum):  # noqa: N801
+    """Available profile scopes to be used in the WCS API."""
+
     SYSTEM_WIDE = 0
     CURRENT_USER = 1
 
 
 class COLORPROFILETYPE(IntEnum):
+    """Available color profile types to be used in the COLORPROFILETYPE field."""
+
     CPT_ICC = 0
     CPT_DMP = auto()
     CPT_CAMP = auto()
@@ -43,6 +54,8 @@ class COLORPROFILETYPE(IntEnum):
 
 
 class COLORPROFILESUBTYPE(IntEnum):
+    """Available color profile subtypes to be used in the COLORPROFILESUBTYPE field."""
+
     # intent
     CPST_PERCEPTUAL = 0
     CPST_RELATIVE_COLORIMETRIC = auto()
@@ -54,4 +67,3 @@ class COLORPROFILESUBTYPE(IntEnum):
     CPST_CUSTOM_WORKING_SPACE = auto()
     CPST_STANDARD_DISPLAY_COLOR_MODE = auto()
     CPST_EXTENDED_DISPLAY_COLOR_MODE = auto()
-
